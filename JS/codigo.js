17ad4fccf6291338967f61884dd85a69f73fffd0
Martin Amarfil <amarfil05@gmail.com>
2024-01-08 08:56:22 -0300

    Segunda entrega

--- JS/codigo.js
+++ JS/codigo.js
@@ -1,139 +1,118 @@
+let Stock_Productos = [];
+const tabla_HTML=document.getElementById("Listado_Productos");
 
-const InicializarStock=(Stock)=>{
-let listadoOpciones = "Elija el producto al que quiere modificar el stock:\n\n"
-for(let x in Stock) {
-    listadoOpciones+= parseInt(x)+1 + ") " + Stock[x][0] + "\n"
-    }
-listadoOpciones+="0) Salir"
-let salir=false
-do{
-    opcion = prompt(listadoOpciones)
-    if (opcion!="0" && opcion!=null){
-        let num=parseInt(opcion)
-        if(num>0 && num<=Stock.length){
-            let cantidad = parseInt(prompt("Ingrese la cantidad actual del producto " +Stock[num-1][0]))
-            if(cantidad>=0){
-                Stock[num-1][1]=cantidad
-            }else{
-                alert ("Ingresó un valor incorrecto")
-            }
-        }else {
-            alert("la opcion no existe")
-        }
-    } else {
-        salir=true
-    }
-}while (!salir)
+const Guardar_Stock_Local=()=>{
+    localStorage.setItem("Productos_Guardados",JSON.stringify(Stock_Productos));
 }
 
-const AgregarElemento=(Stock)=>{
-    let listadoOpciones = "Elija el producto al que hay que agregar mas stock:\n\n"
-    for(let x in Stock) {
-        listadoOpciones+= parseInt(x)+1 + ") " + Stock[x][0] + "\n"
+const Actualizar_Stock_Insumo=(fila, valor)=>{
+    if((valor<0) || (typeof(valor)!=="number"))
+        {valor=0;
         }
-    listadoOpciones+="0) Salir"
-    let salir=false
-    do{
-        opcion = prompt(listadoOpciones)
-        if (opcion!="0" && opcion!=null){
-            let num=parseInt(opcion)
-            if(num>0 && num<=Stock.length){
-                let cantidad = parseInt(prompt("Ingrese la cantidad a agregar al producto " +Stock[num-1][0]))
-                if(cantidad>=0){
-                    Stock[num-1][1]+=cantidad
-                }else{
-                    alert ("Ingresó un valor incorrecto")
-                }
-            }else {
-                alert("la opcion no existe")
-            }
-        } else {
-            salir=true
-        }
-    }while (!salir)
+    Stock_Productos[fila][1]=valor;
+    const lista = tabla_HTML.getElementsByClassName("cantidad");
+    lista[fila].innerHTML=valor;
+    Guardar_Stock_Local();
 }
 
-const QuitarElemento=(Stock)=>{
-    let listadoOpciones = "Elija el producto al que hay que descontar stock:\n\n"
-    for(let x in Stock) {
-        listadoOpciones+= parseInt(x)+1 + ") " + Stock[x][0] + "\n"
-        }
-    listadoOpciones+="0) Salir"
-    let salir=false
-    do{
-        opcion = prompt(listadoOpciones)
-        if (opcion!="0" && opcion!=null){
-            let num=parseInt(opcion)
-            if(num>0 && num<=Stock.length){
-                let cantidad = parseInt(prompt("Ingrese la cantidad a quitar al producto " +Stock[num-1][0]))
-                if(cantidad>=0){
-                    Stock[num-1][1]-=cantidad
-                    if (Stock[num-1][1]<0){
-                        alert(Stock[num-1][1] + " elementos se intentaron quitar del stock, pero no existian. El stock queda en cero.")
-                        Stock[num-1][1]=0
-                    }
-                }else{
-                    alert ("Ingresó un valor incorrecto")
-                }
-            }else {
-                alert("la opcion no existe")
-            }
-        } else {
-            salir=true
-        }
-    }while (!salir)
-
+const Eliminar_Insumo=(fila)=>{
+    Stock_Productos.splice(fila,1);
+    Guardar_Stock_Local();
+    Encabezado_Tabla();
+    MostrarProductos();
 }
 
-const VerStock=(Stock)=>{
-let listadoStock="El stock actual es el siguiente: \n \n"
-for(let x in Stock) {
-    listadoStock+= "- " + Stock[x][0] + " hay " + Stock[x][1] + "\n"
-    }
-alert (listadoStock)
-}
-
-
-const MenuPrincipal=()=>{
-let opcion = ""
-let salir=false
-const Stock=[["Computadoras",0],["Mouse",0],["Teclados",0],["Monitores",0]]
-do{
-    opcion = prompt("Control de stock, seleccione una opcion:\n \n 1) Inicializar Stock \n 2) Agregar elementos stock \n 3) Quitar elemento del stock \n 4) Ver stock \n 0) Salir de la aplicacion")
-    if (opcion!="0" && opcion!=null){
-        switch (opcion){
-        case "0":
-            break;
-        case "1":
-            InicializarStock(Stock);
-            break;
-        case "2":
-            AgregarElemento(Stock);
-            break;
-        case "3":
-            QuitarElemento(Stock);
-            break;
-        case "4":
-            VerStock(Stock);
-            break;
-        default:
-            alert ("Opción no existente");
-        }
-    } else {
-        salir=true
-    }
-}while (!salir)
-}
-
-let intentos = 1
-let contrasenia = ""
-do{
-contrasenia = prompt("Introduzca la contraseña para ingresar o Cancele para salir (Intento " + intentos + ")")
-if (contrasenia === "123"){
-    alert("¡¡¡Bienvenido a la aplicación para control de stock!!!!")
-    MenuPrincipal()
-    contrasenia=null
+const InicializarProductos=()=>{
+let auxiliar =localStorage.getItem("Productos_Guardados");
+if(auxiliar===null){
+    Stock_Productos=[["Computadoras",0],["Mouse",0],["Teclados",0],["Monitores",0]];
+    Guardar_Stock_Local();
     } else{
-    intentos++
+    Stock_Productos=JSON.parse(auxiliar);
     }
-}while (contrasenia != null )
+}
+
+const MostrarProductos=()=>{
+    Stock_Productos.forEach((elemento,filaNum) => {
+        const fila =document.createElement("tr");
+        fila.innerHTML= `
+            <td><strong>${elemento[0]}</strong></td>
+            <td class="cantidad">${elemento[1]}</td>
+            <td><input type="button" value="Vaciar"></td>
+            <td><input type="button" value="Sumar">
+                <input type="text" size="4"></td>
+            <td><input type="button" value="Eliminar"></td>
+            `;
+        tabla_HTML.appendChild(fila);
+        const etiqueta=fila.getElementsByTagName("input");
+        //Vaciar
+        etiqueta[0].addEventListener("click",()=>{
+            Actualizar_Stock_Insumo(filaNum,0)
+            });
+        //Agregar
+        etiqueta[1].addEventListener("click",()=>{
+            const cantidad= parseInt(etiqueta[2].value);
+            if (!(isNaN(cantidad))){
+                Actualizar_Stock_Insumo(filaNum,Stock_Productos[filaNum][1] + parseInt(cantidad))
+              } else {
+                alert("Ingrese un valor numerico");
+              }
+            });
+        //eliminar
+        etiqueta[3].addEventListener("click",()=>{
+            Eliminar_Insumo(filaNum);
+            });
+        }
+    );
+}
+
+const Encabezado_Tabla=()=>{
+    tabla_HTML.innerHTML=`
+    <tr style="background-color: rgb(207, 212, 209);">
+    <td><strong>Insumo</strong></td>
+    <td><strong>Cant</strong></td>
+    <td><strong>Vaciar</strong></td>
+    <td><strong>Agregar/Quitar Stock</strong></td>
+    <td><strong>Eliminar insumo</strong></td>
+    </tr>
+    `
+}
+
+const Existe_Insumo=(texto)=>{
+    const texto1=String(texto).toLowerCase();
+    let band =false;
+    if (texto != ""){
+        let x =0;
+        const fin= Stock_Productos.length;
+        while((! band) && (x < fin)){
+            const texto2=String(Stock_Productos[x][0]).toLowerCase();
+            if (texto1 === texto2){
+                band=true;
+                }
+            x++;
+        }
+    }
+    return band
+}
+
+InicializarProductos();
+Encabezado_Tabla();
+MostrarProductos();
+document.getElementById("Boton_Agregar").addEventListener("click",()=>{
+    const texto = document.getElementById("Texto_Insumo_Nuevo");
+    if(texto.value==""){
+        alert("Por favor ingrese el nombre del insumo");
+    } else{
+        if(Existe_Insumo(texto.value)){
+            alert("Ya existe el producto");
+        } else {
+            Stock_Productos.push([texto.value,0]);
+            Guardar_Stock_Local();
+            Encabezado_Tabla();
+            MostrarProductos();
+            texto.value="";
+            }
+        }
+    })
+
+
