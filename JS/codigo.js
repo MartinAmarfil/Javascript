9ce392159ecb6de2fd3c9470beaa4479319d3ad1
Martin Amarfil <amarfil05@gmail.com>
2024-01-26 16:38:02 -0300

    Entrega final V1

--- JS/codigo.js
+++ JS/codigo.js
@@ -1,73 +1,63 @@
-let Stock_Productos = [];
-const tabla_HTML=document.getElementById("Listado_Productos");
+const tablaHTML=document.getElementById("tablaProductos");
 
-const Guardar_Stock_Local=()=>{
-    localStorage.setItem("Productos_Guardados",JSON.stringify(Stock_Productos));
+const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 4000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.onmouseenter = Swal.stopTimer;
+      toast.onmouseleave = Swal.resumeTimer;
+    }
+  });
+
+/*   Toast.fire({
+    icon: "success", //warning, error, success, info, question.
+    title: "Signed in successfully"
+  }); */
+
+class ClaseStock{
+    constructor(){
+        let auxiliar =localStorage.getItem("productosGuardados");
+        this.stockProductos = [];
+        if(auxiliar===null){
+            this.stockProductos=[["Computadoras",0],["Monitores",0],["Mouse",0],["Teclados",0]];
+            this.cantInsumos =4;
+            this.guardarStock();
+        } else {
+            this.stockProductos=JSON.parse(auxiliar);
+            this.cantInsumos = this.stockProductos.length;
+        }
+        this.mostrarProductos();
+    }
+
+guardarStock(){
+    this.ordenarInsumos();
+    localStorage.setItem("productosGuardados",JSON.stringify(this.stockProductos));
 }
 
-const Actualizar_Stock_Insumo=(fila, valor)=>{
+ordenarInsumos(){
+    this.stockProductos.sort(function(a,b){
+        if(String(a[0]).toLowerCase() < String(b[0]).toLowerCase()){return -1;} 
+        if(String(a[0]).toLowerCase() > String(b[0]).toLowerCase()){return 1;} 
+        return 0;
+    })
+}
+
+actualizarStock=(fila, valor)=>{
     if((valor<0) || (typeof(valor)!=="number"))
         {valor=0;
         }
-    Stock_Productos[fila][1]=valor;
-    const lista = tabla_HTML.getElementsByClassName("cantidad");
+    this.stockProductos[fila][1]=valor;
+    const lista = tablaHTML.getElementsByClassName("cantidad");
     lista[fila].innerHTML=valor;
-    Guardar_Stock_Local();
+    this.guardarStock();
+    this.mostrarInformacionInsumos();
 }
 
-const Eliminar_Insumo=(fila)=>{
-    Stock_Productos.splice(fila,1);
-    Guardar_Stock_Local();
-    Encabezado_Tabla();
-    MostrarProductos();
-}
-
-const InicializarProductos=()=>{
-let auxiliar =localStorage.getItem("Productos_Guardados");
-if(auxiliar===null){
-    Stock_Productos=[["Computadoras",0],["Mouse",0],["Teclados",0],["Monitores",0]];
-    Guardar_Stock_Local();
-    } else{
-    Stock_Productos=JSON.parse(auxiliar);
-    }
-}
-
-const MostrarProductos=()=>{
-    Stock_Productos.forEach((elemento,filaNum) => {
-        const fila =document.createElement("tr");
-        fila.innerHTML= `
-            <td><strong>${elemento[0]}</strong></td>
-            <td class="cantidad">${elemento[1]}</td>
-            <td><input type="button" value="Vaciar"></td>
-            <td><input type="button" value="Sumar">
-                <input type="text" size="4"></td>
-            <td><input type="button" value="Eliminar"></td>
-            `;
-        tabla_HTML.appendChild(fila);
-        const etiqueta=fila.getElementsByTagName("input");
-        //Vaciar
-        etiqueta[0].addEventListener("click",()=>{
-            Actualizar_Stock_Insumo(filaNum,0)
-            });
-        //Agregar
-        etiqueta[1].addEventListener("click",()=>{
-            const cantidad= parseInt(etiqueta[2].value);
-            if (!(isNaN(cantidad))){
-                Actualizar_Stock_Insumo(filaNum,Stock_Productos[filaNum][1] + parseInt(cantidad))
-              } else {
-                alert("Ingrese un valor numerico");
-              }
-            });
-        //eliminar
-        etiqueta[3].addEventListener("click",()=>{
-            Eliminar_Insumo(filaNum);
-            });
-        }
-    );
-}
-
-const Encabezado_Tabla=()=>{
-    tabla_HTML.innerHTML=`
+encabezadoTabla(){
+    tablaHTML.innerHTML=`
     <tr style="background-color: rgb(207, 212, 209);">
     <td><strong>Insumo</strong></td>
     <td><strong>Cant</strong></td>
@@ -78,41 +68,103 @@ const Encabezado_Tabla=()=>{
     `
 }
 
-const Existe_Insumo=(texto)=>{
-    const texto1=String(texto).toLowerCase();
-    let band =false;
-    if (texto != ""){
-        let x =0;
-        const fin= Stock_Productos.length;
-        while((! band) && (x < fin)){
-            const texto2=String(Stock_Productos[x][0]).toLowerCase();
-            if (texto1 === texto2){
-                band=true;
-                }
-            x++;
-        }
-    }
-    return band
+eliminarInsumo(fila){
+    this.stockProductos.splice(fila,1);
+    this.cantInsumos--;
+    this.guardarStock();
+    this.encabezadoTabla();
+    this.mostrarProductos();
 }
 
-InicializarProductos();
-Encabezado_Tabla();
-MostrarProductos();
-document.getElementById("Boton_Agregar").addEventListener("click",()=>{
-    const texto = document.getElementById("Texto_Insumo_Nuevo");
-    if(texto.value==""){
-        alert("Por favor ingrese el nombre del insumo");
-    } else{
-        if(Existe_Insumo(texto.value)){
-            alert("Ya existe el producto");
-        } else {
-            Stock_Productos.push([texto.value,0]);
-            Guardar_Stock_Local();
-            Encabezado_Tabla();
-            MostrarProductos();
-            texto.value="";
-            }
+existeInsumo(texto){
+    let band =false;
+    this.stockProductos.forEach((elemento) => {
+        if (texto.toLowerCase() === String(elemento[0]).toLowerCase()){
+            band=true;
         }
-    })
+    });
+    return band;
+}
+
+agregarInsumo(insumo){
+    this.stockProductos.push([insumo,0]);
+    this.cantInsumos++;
+    this.guardarStock();
+    this.encabezadoTabla();
+    this.mostrarProductos();
+}
+
+totalStock(){
+    let total=0;
+    this.stockProductos.forEach((elemento)=>{total+=elemento[1]});
+    return total;
+}
+
+mostrarInformacionInsumos(){
+    const informacionInsumos=document.getElementById("informacionInsumos");
+    informacionInsumos.innerHTML=`
+    <strong>Cantidad de Insumos: </strong>${this.cantInsumos}<br> 
+    <strong>Total de stock: </strong>${this.totalStock()}<br> 
+    <br>
+    `;
+}
+
+mostrarProductos(){
+    this.mostrarInformacionInsumos();
+    this.encabezadoTabla();
+    this.stockProductos.forEach((elemento,filaNum) => {
+        const fila =document.createElement("tr");
+        fila.innerHTML= `
+            <td><strong>${elemento[0]}</strong></td>
+            <td class="cantidad">${elemento[1]}</td>
+            <td><input type="button" value="Vaciar"></td>
+            <td><input type="button" value="Sumar">
+                <input type="text" size="4"></td>
+            <td><input type="button" value="Eliminar"></td>
+            `;
+        tablaHTML.appendChild(fila);
+        const etiqueta=fila.getElementsByTagName("input");
+        
+        //Vaciar
+        etiqueta[0].addEventListener("click",()=>{
+            this.actualizarStock(filaNum,0)
+        });
+        
+        //Agregar
+        etiqueta[1].addEventListener("click",()=>{
+            const cantidad= parseInt(etiqueta[2].value);
+            if (!(isNaN(cantidad))){
+                this.actualizarStock(filaNum,this.stockProductos[filaNum][1] + parseInt(cantidad))
+            } else {
+                Toast.fire({icon: "warning", title: "Ingrese un valor numerico"});
+            }
+        });
+        
+        //eliminar
+        etiqueta[3].addEventListener("click",()=>{
+            this.eliminarInsumo(filaNum);
+        });
+
+        }
+    );
+}
+
+}
+
+const stock = new ClaseStock();
+
+document.getElementById("botonAgregar").addEventListener("click",()=>{
+    const texto = document.getElementById("insumoNuevo");
+    if(texto.value==""){
+        Toast.fire({icon: "error", title: "Por favor ingrese el nombre del insumo"});
+    } else{
+        if(stock.existeInsumo(texto.value)){
+            Toast.fire({icon: "error", title: "Ya existe el producto: " + texto.value});
+        } else {
+            stock.agregarInsumo(texto.value);
+            texto.value="";
+        }
+    }
+})
 
 
